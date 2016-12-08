c9db53a523f7191b0ec0f04c1de37368e5d5faec
rameshkolamala <rameshzce@gmail.com>
2016-12-08 06:38:07 +0530

    events changes

--- app/src/main/java/com/tokkalo/nzta/SpecialAdapter2.java
+++ app/src/main/java/com/tokkalo/nzta/SpecialAdapter2.java
@@ -32,6 +32,7 @@ public class SpecialAdapter2 extends SimpleAdapter {
     private int[] listItemBackground = new int[]{R.drawable.list_background4, R.drawable.list_background4};
     private int[] galleryBackground = new int[]{R.drawable.gallery_bg10, R.drawable.gallery_bg10};
     private int[] eventImage = new int[]{R.drawable.icon_batukamma, R.drawable.icon_blooddonations};
+    private int[] iconImages = new int[]{R.drawable.icon_calander_inagaration, R.drawable.icon_sankranthi, R.drawable.icon_independeneceday, R.drawable.icon_blooddonations, R.drawable.icon_holi, R.drawable.icon_ugadi, R.drawable.icon_independeneceday, R.drawable.icon_vinayakachaviti, R.drawable.icon_batukamma, R.drawable.icon_diwali, R.drawable.icon_vanabojanalu, R.drawable.icon_xmas};
 
     public SpecialAdapter2(Context context, List<HashMap<String, String>> items, int resource, String[] from, int[] to, String year) {
         super(context, items, resource, from, to);
@@ -62,7 +63,7 @@ public class SpecialAdapter2 extends SimpleAdapter {
         //ll.setBackgroundResource(listItemBackground[colorPos]);
         tr2.setBackgroundResource(galleryBackground[pos]);
         tv.setPadding(padding[pos], 0, 20, 0);
-        img.setBackgroundResource(eventImage[pos]);
+        img.setBackgroundResource(iconImages[position]);
 
 
         view.setMinimumHeight(90);
