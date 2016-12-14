3be9f81eac8bd257845d6cf7aeab745d9195e383
rameshkolamala <rameshzce@gmail.com>
2016-12-15 04:25:46 +0530

    firebase messaging changes

---
+++ app/src/main/java/com/tokkalo/nzta/MyFirebaseMessagingService.java
@@ -0,0 +1,54 @@
+package com.tokkalo.nzta;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by rameshkolamala on 15/12/16.
+ */
+public class MyFirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    // [START receive_message]
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        showNotification(remoteMessage.getData().get("message"));
+    }
+
+    private void showNotification(String message) {
+
+        Intent i = new Intent(this, ShowNotificationActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setAutoCancel(true)
+                .setContentTitle("FCM Test")
+                .setContentText(message)
+                .setSmallIcon(R.drawable.icon_batukamma)
+                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        manager.notify(0, builder.build());
+    }
+}
