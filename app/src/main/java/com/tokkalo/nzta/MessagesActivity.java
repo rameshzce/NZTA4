d44015d30c3c4321aeea3c1adef083fb4ba4b279
rameshkolamala <rameshzce@gmail.com>
2016-12-22 05:57:20 +0530

    message changes

--- app/src/main/java/com/tokkalo/nzta/MessagesActivity.java
+++ app/src/main/java/com/tokkalo/nzta/MessagesActivity.java
@@ -1,13 +1,120 @@
 package com.tokkalo.nzta;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.widget.AbsListView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MessagesActivity extends AppCompatActivity {
+    Context applicationContext;
+    private static final String TAG_RESULTS = "result";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_ADD = "address";
+    SharedPreferences prefs;
+
+    String year;
+
+    String jsonData = "{\"status\":\"SUCCESS\",\"message\":\"9 doner(s) found for your search.\",\"remarks\":\"\",\"doners\":{\"result\":[{\"id\":\"Calender Inauguration\",\"name\":1,\"address\":\"Rangoli competition & kite festival on 17-1-2016 \"},{\"id\":\"Sankranthi\",\"name\":2,\"address\":\"Event at epsom on 1-3-2016 Saturday at 6.00pm, all are welcome and followed by dinner \"},{\"id\":\"Republic Day\",\"name\":3,\"address\":\"War memorial hall, Mount eden on 1-6-2016 Friday 6.00pm, all are welcome and followed by dinner.\"},{\"id\":\"Blood Donations\",\"name\":4,\"address\":\"Diwali stall opens at 2 Pm at queens street. reworks and programs starts at 7 Pm.\\r\\nThe next day we have diwali celabrations\\r\\nat avondale. children participating Dances and some programs. please participate and enjoy the celebrations at 7.00 PM. Followed \"},{\"id\":\"Holi Funday\",\"name\":5,\"address\":\"War memorial hall, Mount eden,\\r\\non 01-06-16 friday, 6.00 pm, all are welcome and followed by dinner.\"},{\"id\":\"Ugadi\",\"name\":6,\"address\":\"Rangoli competation & kite festival on 17-01-16 sunday.\"},{\"id\":\"Independence Day\",\"name\":7,\"address\":\"Eevent at epsom on 01-03-16 Saturday at 6.00 pm, all are welcome\\r\\nand followed by dinner.\"},{\"id\":\"Vinayaka Chaviti\",\"name\":8,\"address\":\"War memorial hall, Mount eden,\\r\\non 01-06-16 friday, 6.00 pm, all are welcome and followed by dinner.\"},{\"id\":\"Batukamma\",\"name\":9,\"address\":\"Diwali stall  \"},{\"id\":\"Dasara & Diwali\",\"name\":10,\"address\":\"Diwali stall  \"},{\"id\":\"Vanabojanalu\",\"name\":11,\"address\":\"Diwali stall  \"},{\"id\":\"Xmas\",\"name\":12,\"address\":\"Diwali stall  \"}]}}";
+    JSONArray peoples = null;
+
+    ArrayList<HashMap<String, String>> personList;
+
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
+
+        applicationContext = getApplicationContext();
+
+        ActionBar ab = getSupportActionBar();
+
+
+        ab.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#fc5f22")));
+
+        TextView tv = new TextView(getApplicationContext());
+
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                AbsListView.LayoutParams.MATCH_PARENT, // Width of TextView
+                AbsListView.LayoutParams.WRAP_CONTENT); // Height of TextView
+
+        tv.setLayoutParams(lp);
+
+
+        tv.setText("Messages");
+
+
+        tv.setGravity(Gravity.CENTER);
+
+        //tv.setTypeface(font);
+
+        tv.setTextColor(Color.parseColor("#FFFFFF"));
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+
+        ab.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+
+        ab.setCustomView(tv);
+
+        list = (ListView) findViewById(R.id.listView);
+        personList = new ArrayList<HashMap<String, String>>();
+
+
+        showList(year);
+    }
+
+    protected void showList(String year) {
+        try {
+            //JSONObject jsonObj = new JSONObject(myJSON);
+            JSONObject jsonObj1 = new JSONObject(jsonData);
+            String events = jsonObj1.getString("doners");
+
+            JSONObject jsonObj = new JSONObject(events);
+            peoples = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for (int i = 0; i < peoples.length(); i++) {
+                JSONObject c = peoples.getJSONObject(i);
+                String id = c.getString(TAG_ID);
+                String address = c.getString(TAG_ADD);
+
+                HashMap<String, String> persons = new HashMap<String, String>();
+
+                persons.put(TAG_ID, id);
+                persons.put(TAG_ADD, address);
+
+                personList.add(persons);
+            }
+
+            SpecialAdapter2 adapter = new SpecialAdapter2(
+                    MessagesActivity.this, personList, R.layout.list_item2,
+                    new String[]{TAG_ID, TAG_ADD},
+                    new int[]{R.id.id, R.id.address}, year
+            );
+
+            list.setAdapter(adapter);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
